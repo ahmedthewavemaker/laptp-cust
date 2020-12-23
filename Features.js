f162528526f1475bef2e31034e47bb39d0b63d81
ahmedthewavemaker <72584721+ahmedthewavemaker@users.noreply.github.com>
2020-12-23 15:12:44 -0500

    Add files via upload

--- Features.js
+++ Features.js
@@ -8,39 +8,43 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 });
 
 
-class Features extends Component{
-render(){
-const features = Object.keys(this.props.features).map((feature, idx) => {
-    const featureHash = feature + '-' + idx;
-    const options = this.props.features[feature].map(item => {
-      const itemHash = slugify(JSON.stringify(item));
-      return (
-        <div key={itemHash} className="feature__item">
-          <input
-            type="radio"
-            id={itemHash}
-            className="feature__option"
-            name={slugify(feature)}
-            checked={item.name === this.props.selected[feature].name}
-            onChange={e => this.updateFeature(feature, item)}
-          />
-          <label htmlFor={itemHash} className="feature__label">
-            {item.name} ({USCurrencyFormat.format(item.cost)})
-          </label>
-        </div>
-      );
-    }); 
+class Features extends Component {
+    render() {
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+                const itemHash = slugify(JSON.stringify(item));
+            })
+        })
 
-    return (
-      <fieldset className="feature" key={featureHash}>
+        return (
+            <div key={itemHash} className="feature__item">
+                <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(feature)}
+                    checked={item.name === this.props.selected[feature].name}
+                    onChange={e => this.updateFeature(feature, item)}
+                />
+                <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({USCurrencyFormat.format(item.cost)})
+          </label>
+            </div>
+        )
+    
+
+
+return (
+    <fieldset className="feature" key={featureHash}>
         <legend className="feature__name">
-          <h3>{feature}</h3>
+            <h3>{feature}</h3>
         </legend>
         {options}
-      </fieldset>
-    );
-  });
+    </fieldset>
+)
 }
-}
+    }
+
 
 export default Features;
